d688f3876849ea7b0ed03f97b89aad09ca9f6733
Adam Scott <ascott1@gmail.com>
2019-02-06 07:12:20 -0500

    set useFindAndModify globally

--- api/db.js
+++ api/db.js
@@ -3,6 +3,9 @@ const mongoose = require('mongoose');
 module.exports = {
   connect: async DB_HOST => {
     mongoose.set('useNewUrlParser', true);
+    // Make Mongoose use `findOneAndUpdate()`. Note that this option is `true`
+    // by default, you need to set it to false.
+    mongoose.set('useFindAndModify', false);
     await mongoose.connect(DB_HOST);
     mongoose.connection.on('error', err => {
       console.error(err);
