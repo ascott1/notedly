35d1b665b5acc52c41f0cefcaaf3b9d2ff0093aa
Adam Scott <ascott1@gmail.com>
2019-02-06 07:12:40 -0500

    set up jest tests and test all queries

--- api/tests/resolvers/query.test.js
+++ api/tests/resolvers/query.test.js
@@ -1,52 +1,108 @@
-const db = require('../../db');
 const mongoose = require('mongoose');
+
+const db = require('../../db');
 const models = require('../../models');
 const resolvers = require('../../resolvers');
+const seedData = require('../../util/seed');
 require('dotenv').config();
+
+let data;
 
 describe('Queries', () => {
   // Connect to the database
-  beforeAll(() => {
-    return db.connect(process.env.TEST_DB);
+  beforeAll(async () => {
+    db.connect(process.env.TEST_DB);
+    data = await seedData();
+    return data;
   });
-  // Close the database connection when all tests complete
-  afterAll(() => {
-    return db.close();
-  });
-  // Remove all records after each test
-  afterEach(async () => {
+  // Remove all records & close the db connection when all tests complete
+  afterAll(async () => {
     await models.Note.deleteMany({});
     await models.User.deleteMany({});
+    return db.close();
   });
 
   describe('notes', () => {
     test('`singleNote` should return a note', async () => {
-      const user = mongoose.Types.ObjectId();
-      const note = await models.Note.create({
-        content: 'test',
-        htmlContent: '<p>test</p>',
-        favoriteCount: 0,
-        favoritedBy: [],
-        author: user
-      });
+      const note = data.notes[0];
       const result = await resolvers.Query.singleNote(
         null,
         { id: note._id },
         { models }
       );
-
-      expect(result._id + '').toBe(result._id + '');
+      expect(result._id + '').toBe(note._id + '');
     });
-    test('`notes` should resolve all notes in the db', async () => {});
-    test('`notefeed` should return a page of notes', async () => {});
-    test('`myNotes` should return the notes of the current user', async () => {});
-    test('`userFavorites` should return the favorite notes of a given user', async () => {});
-    test('`myFavorites` should return the favorite notes of the current user', async () => {});
+    test('`notes` should resolve all notes in the db', async () => {
+      const result = await resolvers.Query.notes(null, null, { models });
+      expect(result.length).toBe(25);
+    });
+    test('`notefeed` should return a page of notes and metadata', async () => {
+      const result = await resolvers.Query.noteFeed(
+        null,
+        { page: 2 },
+        { models }
+      );
+      expect(result.notes.length).toBe(10);
+      expect(result.page).toBe(2);
+      expect(result.pages).toBe(3);
+      expect(result.total).toBe(25);
+    });
+    test('`myNotes` should return the notes of the current user', async () => {
+      const user = {};
+      user._id = data.notes[0].author;
+      const result = await resolvers.Query.myNotes(null, null, {
+        models,
+        user
+      });
+      expect(result.length).toBeGreaterThanOrEqual(1);
+    });
+    test('`userFavorites` should return the favorite notes of a given user', async () => {
+      const user = {};
+      user._id = data.notes[0].author;
+      noteId = data.notes[0]._id;
+
+      await models.Note.findByIdAndUpdate(noteId, {
+        $push: { favoritedBy: user._id }
+      });
+
+      const result = await resolvers.Query.userFavorites(
+        null,
+        { id: user._id },
+        { models }
+      );
+      expect(result.length).toBeGreaterThanOrEqual(1);
+    });
+    test('`myFavorites` should return the favorite notes of the current user', async () => {
+      const user = {};
+      user._id = data.notes[2].author;
+      noteId = data.notes[2]._id;
+
+      await models.Note.findByIdAndUpdate(noteId, {
+        $push: { favoritedBy: user._id }
+      });
+
+      const result = await resolvers.Query.myFavorites(null, null, {
+        models,
+        user
+      });
+      expect(result.length).toBeGreaterThanOrEqual(1);
+    });
   });
 
   describe('users', () => {
     test('`singleUser` should resolve a user', async () => {});
-    test('`allUsers` should resolve all users in the db', async () => {});
-    test('`me` should resolve the current user info', async () => {});
+    test('`allUsers` should resolve all users in the db', async () => {
+      const result = await resolvers.Query.allUsers(null, null, { models });
+      expect(result.length).toBe(10);
+    });
+    test('`me` should resolve the current user info', async () => {
+      const user = {};
+      user._id = data.notes[0].author;
+      const result = await resolvers.Query.me(null, null, {
+        models,
+        user
+      });
+      expect(result.name).toBeTruthy();
+    });
   });
 });
